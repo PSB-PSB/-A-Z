4ad35f95666fd73a0b1b366595322bc4ffc8d6ad
yatey <yatey@DESKTOP-3809L1R>
2024-02-16 23:24:56 +0900

    폴더 년/월/일 생성 작업 / UUID를 이용한 중복 방지

--- bookcafe/src/main/java/org/zerock/controller/UploadController.java
+++ bookcafe/src/main/java/org/zerock/controller/UploadController.java
@@ -1,6 +1,9 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,12 +22,23 @@ public class UploadController {
 		log.info("upload form");
 	}
 	
+	private String getForlder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = new Date();
+		
+		String str = sdf.format(date);
+		
+		return str.replace("-", File.separator);
+	}
+	
 	@PostMapping("/uploadFormAction")
 	public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
 		
-		String uploadFolder = "C:\\upload";
+			String uploadFolder = "C:\\upload";
 		
 		for(MultipartFile multipartFile : uploadFile) {
+			
 			log.info("----------------");
 			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
 			log.info("Upload File Size : " + multipartFile.getSize());
@@ -37,7 +51,7 @@ public class UploadController {
 				log.info(e.getMessage());
 			}
 		}
-	}
+	} // end uploadFormPost
 	
 	@GetMapping("/uploadAjax")
 	public void uploadAjax() {
@@ -49,6 +63,15 @@ public class UploadController {
 		log.info("update ajax post...");
 		
 		String uploadFolder = "C:\\upload";
+		
+		//폴더 만드는 부분 -------
+		File uploadPath = new File(uploadFolder, getForlder());
+		log.info("업로드 경로 : " + uploadPath);
+		
+		if(uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
+		// 년 월 일 폴더만들기
 		
 		for(MultipartFile multipartFile : uploadFile) {
 			log.info("-------------");
@@ -62,7 +85,14 @@ public class UploadController {
 			
 			log.info("only file name : " + uploadFileName);
 			
-			File saveFile = new File(uploadFolder, uploadFileName);
+			//중복 방지 UUID
+			UUID uuid = UUID.randomUUID();
+			
+			//파일 이름에 하이픈 추가해서 중복 파일 이름 방지.
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			//년 월 일에 생성된 파일저장.
+			File saveFile = new File(uploadPath, uploadFileName);
 			
 			try {
 				multipartFile.transferTo(saveFile);
@@ -71,5 +101,6 @@ public class UploadController {
 			}
 		}
 		
-	}
+	} // end uploadAjaxAction
+	
 }
